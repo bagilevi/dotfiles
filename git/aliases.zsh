6267d6f55c6ceba66695ffcc90b9e726dd6341b5
Zach Holman <zach@zachholman.com>
2012-11-07 11:32:27 +0000

    oh-my-zsh and my git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,11 +9,16 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl="glog -n 10"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gitd='git diff'
 alias gc='git commit'
+alias gitc='git commit -m'
 alias gca='git commit -a'
+alias gitca='git commit -a -m'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gits='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
